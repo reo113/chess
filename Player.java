31974382c07b756c0ad631844f91536681fcb6f2
arteeguz <118378371+arteeguz@users.noreply.github.com>
2023-04-24 20:04:38 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,21 @@
+package application;
 
 public class Player {
-System.out.println("hellow");
+
+    private String name;
+    private boolean isBlack;
+    //timer
+
+    public Player(String name, boolean isBlack) {
+        this.name = name;
+        this.isBlack = isBlack;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isBlack() {
+        return isBlack;
+    }
 }
