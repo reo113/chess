cc792ea3ab7b1c24c26ec7a3d509ce2736a5b973
Jawad Rada <103535961+jawadrada@users.noreply.github.com>
2023-04-26 20:48:39 -0400

    Create Knight.java

---
+++ Knight.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.ArrayList;
+
+public class Knight extends Piece {
+
+	Knight(Boolean isKilled, Boolean whitePiece) {
+		super(isKilled, whitePiece);
+		
+	}
+	
+	@Override
+	public ArrayList<Move> legalMoves(ChessBoard board, Spot start) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
