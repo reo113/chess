88af177b78f8c13bb1d2e5f53b88afcf0b5bab05
Jawad Rada <103535961+jawadrada@users.noreply.github.com>
2023-04-26 20:49:17 -0400

    Create Rook.java

---
+++ Rook.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.ArrayList;
+
+public class Rook extends Piece {
+
+	Rook(Boolean isKilled, Boolean whitePiece) {
+		super(isKilled, whitePiece);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ArrayList<Move> legalMoves(ChessBoard board, Spot start) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
