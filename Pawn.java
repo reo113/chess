cc8e7249e74099b4366dba550f918bf002838302
Jawad Rada <103535961+jawadrada@users.noreply.github.com>
2023-04-26 20:51:16 -0400

    Create Pawn.java

---
+++ Pawn.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.ArrayList;
+
+public class Pawn extends Piece {
+
+	Pawn(Boolean isKilled, Boolean whitePiece) {
+		super(isKilled, whitePiece);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ArrayList<Move> legalMoves(ChessBoard board, Spot start) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
