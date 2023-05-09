1805546b2fc3ce15d8d336331c050fdfb76ef315
reo113 <92471558+reo113@users.noreply.github.com>
2023-05-09 16:31:40 -0400

    Delete Move.java

--- Move.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public interface Move {
-  
-    public boolean isValidMove(int startX, int startY, int endX, int endY);
-    public void makeMove(int startX, int startY, int endX, int endY);
-    public void undoMove(int startX, int startY, int endX, int endY);
-    
-}
